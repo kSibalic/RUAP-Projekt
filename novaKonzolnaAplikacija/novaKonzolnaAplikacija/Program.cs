fe89f55720a10edece4012c7125c6645435abe43
kSibalic <sibalickarlo@gmail.com>
2024-11-04 15:11:30 +0100

    Created new console application

---
+++ novaKonzolnaAplikacija/novaKonzolnaAplikacija/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace novaKonzolnaAplikacija
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
